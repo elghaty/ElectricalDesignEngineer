c531061584a881056a702562b2771fc0acf4c4bc
elghaty <elghaty@gmail.com>
2026-09-05 13:34:15 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -22,11 +23,22 @@ android {
 }
 
 dependencies {
+
+    // Android Core
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
+
+    // Jetpack Compose
     implementation("androidx.compose.ui:ui:1.7.6")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling:1.7.6"
+    )
+
+    // Room Database
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
