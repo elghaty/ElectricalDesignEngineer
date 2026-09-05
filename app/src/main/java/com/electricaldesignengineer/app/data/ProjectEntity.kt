25a858dc4366439e0cb99f1bd0f4677708958bd4
elghaty <elghaty@gmail.com>
2026-09-05 15:04:01 +0300

    Add electrical parameters to ProjectEntity

--- app/src/main/java/com/electricaldesignengineer/app/data/ProjectEntity.kt
+++ app/src/main/java/com/electricaldesignengineer/app/data/ProjectEntity.kt
@@ -16,5 +16,33 @@ data class ProjectEntity(
 
     val engineerName: String,
 
-    val createdDate: String
+    val createdDate: String,
+
+    val connectedKW: Double = 0.0,
+
+    val demandKW: Double = 0.0,
+
+    val totalKVA: Double = 0.0,
+
+    val designCurrentA: Double = 0.0,
+
+    val voltageV: Double = 400.0,
+
+    val powerFactor: Double = 0.90,
+
+    val cableSizeMm2: Double = 0.0,
+
+    val voltageDropPercent: Double = 0.0,
+
+    val shortCircuitKA: Double = 0.0,
+
+    val breakerRatingA: Int = 0,
+
+    val breakerIcuKA: Double = 0.0,
+
+    val transformerKVA: Double = 0.0,
+
+    val generatorKVA: Double = 0.0,
+
+    val capacitorKVAR: Double = 0.0
 )
