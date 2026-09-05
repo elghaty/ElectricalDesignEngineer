e7ca5cf87322f9fc883c3c0760d1ffd29b8afdde
elghaty <elghaty@gmail.com>
2026-09-05 15:08:49 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,20 @@ kotlin {
     jvmToolchain(17)
 }
 
-android { 
+android {
     namespace = "com.electricaldesignengineer.app"
+
     compileSdk = 35
 
     defaultConfig {
         applicationId = "com.electricaldesignengineer.app"
+
         minSdk = 24
+
         targetSdk = 35
+
         versionCode = 1
+
         versionName = "1.0"
     }
 
@@ -38,17 +43,24 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.activity:activity-compose:1.10.0")
 
     implementation("androidx.compose.ui:ui:1.7.6")
+
     implementation("androidx.compose.material3:material3:1.3.1")
+
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
 
     debugImplementation(
         "androidx.compose.ui:ui-tooling:1.7.6"
     )
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
     implementation("androidx.room:room-runtime:2.6.1")
+
     implementation("androidx.room:room-ktx:2.6.1")
+
     kapt("androidx.room:room-compiler:2.6.1")
 }
