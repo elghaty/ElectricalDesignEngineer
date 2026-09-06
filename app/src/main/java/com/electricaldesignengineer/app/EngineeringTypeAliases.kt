79018466838821dc1e009c03efbd0318ef3c4b4c
elghaty <elghaty@gmail.com>
2026-09-06 13:58:54 +0300

    Update EngineeringTypeAliases.kt

--- app/src/main/java/com/electricaldesignengineer/app/EngineeringTypeAliases.kt
+++ app/src/main/java/com/electricaldesignengineer/app/EngineeringTypeAliases.kt
@@ -1,1 +1,10 @@
+package com.electricaldesignengineer.app
 
+typealias InstallationMethod =
+    EngineeringDesignEngine.InstallationMethod
+
+typealias CableMaterial =
+    EngineeringDesignEngine.CableMaterial
+
+typealias InsulationType =
+    EngineeringDesignEngine.InsulationType
